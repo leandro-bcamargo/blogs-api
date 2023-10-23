293247e00e95e9c279deb39c0f03a84cf5ca37bd
Leandro Camargo <leandro.bcamargo@gmail.com>
2023-10-23 10:21:34 -0300

    requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, datatypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: datatypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: {
+      type: datatypes.STRING(255),
+      allowNull: false,
+    },
+    email: {
+      type: datatypes.STRING(255),
+      allowNull: false,
+    },
+    password: {
+      type: datatypes.STRING(255),
+      allowNull: false,
+    },
+    image: {
+      type: datatypes.STRING(255),
+      allowNull: false,
+    }
+  }, {
+    underscored: true,
+    timestamps: false,
+    tableName: 'users',
+  })
+
+  return User;
+}
