ec12a10487acb3c6cd53afdfdba1c173b37d8104
Leandro Camargo <leandro.bcamargo@gmail.com>
2023-10-25 10:56:50 -0300

    requisito 7

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, datatypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: datatypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: datatypes.STRING(256),
+      allowNull: false,
+    }
+  }, {
+    underscored: true,
+    timestamps: false,
+    tableName: 'categories',
+  })
+
+  return Category;
+}
